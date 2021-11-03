268e07ead77a03de0ea5bbc2a330f90bb08bc613
Helen Sung <helenjeehosung@gmail.com>
2021-11-02 23:43:33 -0400

    test: all clear and backspace functionality

--- src/App.test.js
+++ src/App.test.js
@@ -1,35 +1,22 @@
 import React from "react";
-import { mount, shallow, configure } from "enzyme";
+import { mount, configure } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import App from "./App";
 
 configure({ adapter: new Adapter() });
 
+let wrapper;
+beforeEach(() => (wrapper = mount(<App />)));
+
 describe("<App> component can validate user input: ", () => {
-  let wrapper;
-
-  beforeEach(() => (wrapper = mount(<App />)));
-
-  it("valid inputs will produce a result", () => {
-    // console.log(wrapper.debug());
-    wrapper.find('button[value="2"]').simulate("click");
-    wrapper.find('button[value=" - "]').simulate("click");
-    wrapper.find('button[value=" - "]').simulate("click");
-    wrapper.find('button[value="3"]').simulate("click");
-
-    expect(wrapper.find("p").text()).toBe("2 -  - 3");
-    wrapper.find('button[value="="]').simulate("click");
-    expect(wrapper.find("p").text()).toBe("5");
-  });
-
   it("invalid order of parenthesis will produce an alert", () => {
     window.alert = jest.fn(); // mock alert method within testing blackbox
 
-    wrapper.find('button[value=" ) "]').simulate("click");
+    wrapper.find('button[value=")"]').simulate("click");
     wrapper.find('button[value="2"]').simulate("click");
-    wrapper.find('button[value=" - "]').simulate("click");
+    wrapper.find('button[value="-"]').simulate("click");
     wrapper.find('button[value="1"]').simulate("click");
-    wrapper.find('button[value=" ( "]').simulate("click");
+    wrapper.find('button[value="("]').simulate("click");
     wrapper.find('button[value="="]').simulate("click");
 
     expect(window.alert).toHaveBeenCalledWith(
@@ -41,11 +28,11 @@ describe("<App> component can validate user input: ", () => {
   it("Unbalanced parenthesis will produce an alert", () => {
     window.alert = jest.fn(); // mock alert method within testing blackbox
 
-    wrapper.find('button[value=" ( "]').simulate("click");
+    wrapper.find('button[value="("]').simulate("click");
     wrapper.find('button[value="2"]').simulate("click");
-    wrapper.find('button[value=" - "]').simulate("click");
+    wrapper.find('button[value="-"]').simulate("click");
     wrapper.find('button[value="1"]').simulate("click");
-    wrapper.find('button[value=" ( "]').simulate("click");
+    wrapper.find('button[value="("]').simulate("click");
     wrapper.find('button[value="="]').simulate("click");
 
     expect(window.alert).toHaveBeenCalledWith(
@@ -53,13 +40,13 @@ describe("<App> component can validate user input: ", () => {
     );
   });
 
-  it("no more than two operators in a series will be supported", () => {
+  it("no more than two operators in a series will be supported...", () => {
     window.alert = jest.fn(); // mock alert method within testing blackbox
 
     wrapper.find('button[value="2"]').simulate("click");
-    wrapper.find('button[value=" + "]').simulate("click");
-    wrapper.find('button[value=" - "]').simulate("click");
-    wrapper.find('button[value=" + "]').simulate("click");
+    wrapper.find('button[value="+"]').simulate("click");
+    wrapper.find('button[value="-"]').simulate("click");
+    wrapper.find('button[value="+"]').simulate("click");
     wrapper.find('button[value="1"]').simulate("click");
     wrapper.find('button[value="="]').simulate("click");
 
@@ -68,9 +55,43 @@ describe("<App> component can validate user input: ", () => {
     );
   });
 
-  //calculate
+  it("...however consecutive (-) operators are supported", () => {
+    // console.log(wrapper.debug());
+    wrapper.find('button[value="2"]').simulate("click");
+    wrapper.find('button[value="-"]').simulate("click");
+    wrapper.find('button[value="-"]').simulate("click");
+    wrapper.find('button[value="3"]').simulate("click");
 
-  //backspakce
-
-  //clear all
+    expect(wrapper.find("p").text()).toBe("2--3");
+    wrapper.find('button[value="="]').simulate("click");
+    expect(wrapper.find("p").text()).toBe("5");
+  });
 });
+
+//backspace
+describe("<App> component can handle various functionalities from <Button> component:", () => {
+  it("backspace will delete one element at a time", () => {
+    wrapper.find('button[value="2"]').simulate("click");
+    wrapper.find('button[value="2"]').simulate("click");
+    wrapper.find('button[value="x"]').simulate("click");
+    wrapper.find('button[value="5"]').simulate("click");
+    wrapper.find('button[value="DEL"]').simulate("click");
+    expect(wrapper.find("p").text()).toBe("22x");
+    wrapper.find('button[value="DEL"]').simulate("click");
+    expect(wrapper.find("p").text()).toBe("22");
+    wrapper.find('button[value="DEL"]').simulate("click");
+    expect(wrapper.find("p").text()).toBe("");
+  });
+
+  it("AC will clear entire entry (All Clear)", () => {
+    wrapper.find('button[value="2"]').simulate("click");
+    wrapper.find('button[value="2"]').simulate("click");
+    wrapper.find('button[value="x"]').simulate("click");
+    wrapper.find('button[value="5"]').simulate("click");
+    expect(wrapper.find("p").text()).toBe("22x5");
+    wrapper.find('button[value="AC"]').simulate("click");
+    expect(wrapper.find("p").text()).toBe("");
+  });
+});
+
+//calculate
